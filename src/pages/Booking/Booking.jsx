9f2c4c11ca519272f2f1db328a19d9738a58847f
GumayuLong <tothanhlong2002@gmail.com>
2023-10-03 16:24:57 +0700

    update ui booking

--- src/pages/Booking/Booking.jsx
+++ src/pages/Booking/Booking.jsx
@@ -3,6 +3,7 @@ import { useNavigate, useParams } from "react-router-dom";
 import { ticketService } from "../../services/ticket";
 import { filter, sumBy } from "lodash";
 import { LoadingContext } from "../../contexts/LoadingContext/LoadingContext";
+import "./booking.scss"
 
 export default function Booking() {
   const params = useParams();
@@ -79,17 +80,17 @@ export default function Booking() {
 
     // console.log(data);
     return data.map((element) => {
-      return <p className="badge badge-success mr-2 mb-0">{element.tenGhe}</p>;
+      return <p className="badge badge-success mr-2 mb-0">Ghế {element.tenGhe}</p>;
     });
   };
 
   const renderTotalPrice = () => {
     const data = chairList.filter((element) => element.dangChon);
-    // const total = data.reduce((total, element) => total += element.giaVe, 0);
 
     const total = sumBy(data, "giaVe");
 
-    return total.toLocaleString();
+    return <div>{total.toLocaleString()}vnd</div>;
+    
   };
 
   const handleBookTicket = async () => {
@@ -111,47 +112,61 @@ export default function Booking() {
   };
 
   return (
-    <div className="py-5">
-      <div className="row">
-        <div className="col-8 mb-4">
-          <div style={{ width: "95%" }} className="mx-auto">
-            <div className="mr-2 mb-1 d-inline-block p-2 rounded text-white bg-secondary">
-              GHẾ ĐÃ ĐẶT
-            </div>
-            <div className="mr-2 mb-1 d-inline-block p-2 rounded text-white bg-dark">
-              GHẾ TRỐNG
-            </div>
-            <div className="mr-2 mb-1 d-inline-block p-2 rounded text-white bg-success">
-              GHẾ ĐANG CHỌN
-            </div>
-            <div className="mr-2 mb-1 d-inline-block p-2 rounded text-white bg-warning">
-              GHẾ VIP
-            </div>
-          </div>
-        </div>
-        <div className="col-8">
-          <div style={{ width: "95%" }} className="mx-auto">
-            {renderChairList()}
-          </div>
-        </div>
+		<div className="py-5 bgImg">
+			<div className="row">
+				<div className="col-8 mb-4">
+					<div style={{ width: "95%" }} className="mx-auto">
+						<div className="mr-2 mb-1 d-inline-block p-2 rounded text-white bg-secondary">
+							GHẾ ĐÃ ĐẶT
+						</div>
+						<div className="mr-2 mb-1 d-inline-block p-2 rounded text-white bg-dark">
+							GHẾ TRỐNG
+						</div>
+						<div className="mr-2 mb-1 d-inline-block p-2 rounded text-white bg-success">
+							GHẾ ĐANG CHỌN
+						</div>
+						<div className="mr-2 mb-1 d-inline-block p-2 rounded text-white bg-warning">
+							GHẾ VIP
+						</div>
+					</div>
+				</div>
+				<div className="col-8">
+					<div style={{ width: "95%" }} className="mx-auto">
+						{renderChairList()}
+					</div>
+				</div>
 
-        <div className="col-4">
-          <img
-            style={{ width: 300, height: 400, objectFit: "cover" }}
-            src={movieDetail.hinhAnh}
-            alt="#"
-          />
-          <h4 className="mb-0">{movieDetail.tenPhim}</h4>
-          <h5 className="mb-0">
-            Số ghế:
-            <div className="d-flex">{renderSeatList()}</div>
-          </h5>
-          <h5>Tổng tiền: {renderTotalPrice()}</h5>
-          <button className="btn btn-info" onClick={handleBookTicket}>
-            ĐẶT VÉ
-          </button>
-        </div>
-      </div>
-    </div>
+				<div className="col-4">
+					<div className="bgCol4">
+						<img
+							style={{
+								width: 360,
+								height: 400,
+								objectFit: "cover",
+								borderRadius: "10px",
+							}}
+							src={movieDetail.hinhAnh}
+							alt="#"
+						/>
+						<h4 className="mb-0 filmTitle">
+							{movieDetail.tenPhim}
+						</h4>
+						<h5 className="mb-0 selectChair">
+							Số ghế:
+							<div className="d-flex">{renderSeatList()}</div>
+						</h5>
+						<h5 className="total">
+							Tổng tiền: {renderTotalPrice()}
+						</h5>
+						<button
+							className="btn btn-primary"
+							onClick={handleBookTicket}
+						>
+							ĐẶT VÉ
+						</button>
+					</div>
+				</div>
+			</div>
+		</div>
   );
 }
