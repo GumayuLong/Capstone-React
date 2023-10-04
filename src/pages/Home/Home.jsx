020d6a0a629aa81acff34b7b87b0a4cc3637cb4e
GumayuLong <tothanhlong2002@gmail.com>
2023-10-04 21:06:03 +0700

    update home ui - hethongrap and cumrapchieu

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,3 +1,5 @@
+/** @format */
+
 import React, { Fragment, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { movieService } from "../../services/movie";
@@ -11,28 +13,106 @@ import "slick-carousel/slick/slick-theme.css";
 import "./home.scss";
 import { bannerService } from "../../services/banner";
 import { faHandPointRight } from "@fortawesome/free-solid-svg-icons";
+import { cinemaService } from "../../services/cinema";
 
 export default function Home() {
 	const navigate = useNavigate();
+
 	const [movieList, setMovieList] = useState([]);
 	const [bannerList, setBannerList] = useState([]);
+	const [cinema, setCinema] = useState([]);
+	const [heThongCumRap, setHeThongCumRap] = useState([]);
+	const [getListCumRap, setGetListCumRap] = useState({});
+	const [getHeThongLichChieu, setHeThongLichChieu] = useState({})
 
 	useEffect(() => {
 		fetchMovieList();
 		fetchBannerList();
+		fetchGetInfoHeThongRap();
+		fetchGetInfoLichChieuHeThongRap();
 	}, []);
 
 	const fetchMovieList = async () => {
 		const result = await movieService.fetchMovieListApi();
-
 		setMovieList(result.data.content);
 	};
 
 	const fetchBannerList = async () => {
 		const result = await bannerService.fetchBannerListApi();
 		// console.log(result);
-
 		setBannerList(result.data.content);
+	};
+
+	const fetchGetInfoHeThongRap = async () => {
+		const result = await cinemaService.fetchCinemaApi();
+		setCinema(result.data.content);
+	};
+
+	const fetchGetInfoLichChieuHeThongRap = async () => {
+		const result = await cinemaService.fetchGetInfoLichChieuHeThongRapApi();
+		console.log(result.data.content);
+		setGetListCumRap(result.data.content);
+	};
+
+	const handleChangeHeThongRap = (rap) => {
+		const data = [...getListCumRap];
+
+		const idx = data.findIndex(
+			(element) => element.maHeThongRap === rap.maHeThongRap
+		);
+
+		const cumRap = data.filter(
+			(element) =>
+				// console.log(element.maHeThongRap)
+				element.maHeThongRap === data[idx].maHeThongRap
+		);
+
+		setHeThongCumRap(cumRap[0].lstCumRap);
+	};
+
+	// const handleLichChieuCumRap = (rap) => {
+	// 	const data = [...getListCumRap];
+
+	// 	const idx = data.findIndex(
+	// 		(element) => element.maHeThongRap === rap.maHeThongRap
+	// 	);
+
+	// 	const lichChieu = data.filter(
+	// 		(element) =>
+	// 			// console.log(element.maHeThongRap)
+	// 			element.maHeThongRap === data[idx].maHeThongRap
+	// 	);
+
+	// 	console.log(lichChieu[0].lstCumRap);
+	// 	setHeThongLichChieu(lichChieu[0].lstCumRap);
+	// };
+
+	// const renderLichChieu = () => {
+	// 	return heThongCumRap.map((element) => {
+	// 		return <button onClick={handleLichChieuCumRap(element)}></button>;
+	// 	});
+	// }
+
+	const renderCumRap = () => {
+		return heThongCumRap.map((element) => {
+			return (
+				<button
+					className="buttonCustom"
+					style={{ display: "flex", alignItems: "center" }}
+				>
+					<span className="multitabWrapper">
+						<div>
+							<h4 className="htrtitle">{element.tenCumRap}</h4>
+							<h6 className="address">{element.diaChi}</h6>
+							<a className="btnChiTiet" href="/">
+								"[Chi tiết]"
+							</a>
+						</div>
+					</span>
+					{/* <span></span> */}
+				</button>
+			);
+		});
 	};
 
 	const renderMovieList = () => {
@@ -92,6 +172,32 @@ export default function Home() {
 		});
 	};
 
+	const renderListGetInfoHeThongRap = () => {
+		// console.log(cinema)
+		return cinema.map((element) => {
+			return (
+				<div className="borderbutton">
+					<button
+						className="buttonLogo"
+						onClick={() => {
+							handleChangeHeThongRap(element);
+						}}
+					>
+						<span className="multiWrapper">
+							<div className="">
+								<img
+									className="logo"
+									src={element.logo}
+									alt={element.biDanh}
+								/>
+							</div>
+						</span>
+					</button>
+				</div>
+			);
+		});
+	};
+
 	const settings = {
 		dots: true,
 		infinite: true,
@@ -116,6 +222,36 @@ export default function Home() {
 			<div class="mx-auto my-5 w-65">
 				<h1 className="text-center my-5">MOVIE SELECTION</h1>
 				<div className="row">{renderMovieList()}</div>
+			</div>
+			<div
+				className="muiContainer multiContainer"
+				style={{ marginBottom: "90px" }}
+			>
+				<div className="lichchieu">
+					<div className="tabVertical">
+						<div
+							className="multitabsScroller multitabsFix"
+							style={{ overflow: "hidden" }}
+						>
+							<div className="flexContainer">
+								{renderListGetInfoHeThongRap()}
+							</div>
+						</div>
+					</div>
+					{/* <span className="activeSpan"></span> */}
+				</div>
+				<div className="verticalTabpanel">
+					<div className="multitabVertical">
+						<div className="multitabsScroller">
+							<div className="flexContainer">
+								{renderCumRap()}
+							</div>
+						</div>
+					</div>
+				</div>
+				<div>
+					{/* <div>{renderLichChieu()}</div> */}
+				</div>
 			</div>
 		</Fragment>
 	);
--- src/services/cinema.js
+++ src/services/cinema.js
@@ -1,11 +1,25 @@
 import { request } from "../configs/api";
 
 class CinemaService {
-  fetchShowtimesApi(movieId) {
+	fetchShowtimesApi(movieId) {
+		return request({
+			url: `/QuanLyRap/LayThongTinLichChieuPhim?MaPhim=${movieId}`,
+			method: "GET",
+		});
+	}
+
+	fetchCinemaApi() {
+		return request({
+			url: `QuanLyRap/LayThongTinHeThongRap`,
+			method: "GET",
+		});
+	}
+
+  fetchGetInfoLichChieuHeThongRapApi(){
     return request({
-      url: `/QuanLyRap/LayThongTinLichChieuPhim?MaPhim=${movieId}`,
-      method: "GET",
-    });
+		url: `/QuanLyRap/LayThongTinLichChieuHeThongRap?maNhom=GP01`,
+		method: "GET",
+	});
   }
 }
 
